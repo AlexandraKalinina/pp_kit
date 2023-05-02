ce755fd760ca8f2091e1b91efc9379d4a6a27b18
Калинина Александра Владимировна AlVKalinina <alvkalinina@stud.kpfu.ru>
2023-05-02 09:55:51 +0300

    add buy controller

--- ASP.Net_Core_example_app/WebApplication2rrr/Startup.cs
+++ ASP.Net_Core_example_app/WebApplication2rrr/Startup.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
 using WebApplication2rrr.Models;
 
 
